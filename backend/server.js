aaf8a0e64d1c9f9dbc5f7e6c0e12c8e297fe4e48
Sanjay007321 <saniay007aj@gmail.com>
2025-12-25 21:39:49 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ mongoose.connect(process.env.MONGODB_URI);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => {
-  console.log('Connected to MongoDB');
+  console.log('Connected to MDB');
 });
 
 // JWT Secret
